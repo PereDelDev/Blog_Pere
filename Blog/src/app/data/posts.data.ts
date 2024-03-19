68bc8953692fdbe6794a5045b026844645b37188
Pere <pere.pachi9@gmail.com>
2024-03-19 21:12:49 +0100

    Puse imagenes y titulos a los posts

--- Blog/src/app/data/posts.data.ts
+++ Blog/src/app/data/posts.data.ts
@@ -5,55 +5,55 @@ import { Post } from "../interfaces/post.interface";
     export const POSTS: Post[] = [
         {
             id: 1,
-            titulo: 'Primer Post',
+            titulo: 'Angular 17 llega a nuestras vidas',
             texto: 'Esto es un texto de prueba que creo en este array porque no puedo lo que viene a ser meter el lorem ipsum y claro aquí debería de escribir mucho para que salga un buen post que pueda usar yo luego para poner la página un poco diseñada y que no quede tan mal. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'Pere Planells',
-            imagen: 'https://es.web.img3.acsta.net/pictures/19/03/22/10/08/5883111.jpg',
+            imagen: 'https://media.licdn.com/dms/image/D4D12AQGg7hLkz8wD5g/article-cover_image-shrink_720_1280/0/1699876138090?e=2147483647&v=beta&t=JHm5-cXmv-EBQB9xenJhdK_QJKWXTccvrcO571oK2IQ',
             fecha: '2024/03/13',
             categoria: 'Novedades'
         },
         {
             id: 2,
-            titulo: 'Segundo Post',
+            titulo: 'El for Each y porque es mejor',
             texto: 'Este es otro post de ejemplo con un texto similar al anterior pero con contenido distinto. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'Juan Pérez',
-            imagen: 'https://via.placeholder.com/150',
+            imagen: 'https://miro.medium.com/v2/resize:fit:1400/0*lESPAgElCndlGSaF',
             fecha: '2024/03/14',
             categoria: 'Opinión'
         },
         {
             id: 3,
-            titulo: 'Tercer Post',
+            titulo: 'ID Bootcamps surge com revelacion en PingPong Internacional',
             texto: 'Otro post con texto diferente para completar el array. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'María López',
-            imagen: 'https://via.placeholder.com/150',
+            imagen: 'https://iddigitalschool.com/bootcamps/wp-content/uploads/2023/04/final_convocatorias_programacion_data_science_id_bootcamps-e1682671056440.jpeg',
             fecha: '2024/03/15',
             categoria: 'Tecnología'
         },
         {
             id: 4,
-            titulo: 'Cuarto Post',
+            titulo: 'Amsterdam vale la pena o es una santisima mierda?',
             texto: 'Este es otro post de ejemplo con un texto similar al anterior pero con contenido distinto. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'Carlos Martínez',
-            imagen: 'https://via.placeholder.com/150',
+            imagen: 'https://www.milviatges.com/wp-content/uploads/sites/267/2023/03/Como-visitar-el-Barrio-Rojo-de-Amsterdam.jpg',
             fecha: '2024/03/16',
             categoria: 'Opinión'
         },
         {
             id: 5,
-            titulo: 'Quinto Post',
+            titulo: 'Elon mask anuncia su nueva inteligencia artificial, porque la suya se termino',
             texto: 'Otro post con texto diferente para completar el array. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'Ana García',
-            imagen: 'https://via.placeholder.com/150',
+            imagen: 'https://www.semana.com/resizer/lU0bhwogj-Mug66gbsEYfk8e_NE=/arc-anglerfish-arc2-prod-semana/public/NZWX2UC27ZB4TETA3RZLZZG4XE.jpg',
             fecha: '2024/03/17',
             categoria: 'Tecnología'
         },
         {
             id: 6,
-            titulo: 'Sexto Post',
+            titulo: 'Te sientes solo? Pincha aqui!',
             texto: 'Este es otro post de ejemplo con un texto similar al anterior pero con contenido distinto. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit libero nec velit vehicula, vel congue enim finibus. Quisque malesuada neque vitae semper vehicula. Maecenas in lectus justo. Fusce varius hendrerit lacinia. Curabitur id justo et magna convallis auctor. Nullam non velit arcu. Integer a augue ligula. Nam id purus et metus sagittis fringilla ut id purus. Proin a tortor nec orci accumsan tempor non vitae mi. Proin finibus sapien eget ipsum molestie lacinia. Proin rhoncus ultricies dictum. Donec sed leo dolor. Integer et orci non sapien efficitur suscipit vel id lectus.',
             autor: 'Laura Fernández',
-            imagen: 'https://via.placeholder.com/150',
+            imagen: 'https://play-lh.googleusercontent.com/laCWGEefBMyVbnUVc93YtAoQvMdEzcRg2YlKxAlmPhPcb4zuaLXCJPBrwr0p89_hPl8',
             fecha: '2024/03/18',
             categoria: 'Novedades'
         }
